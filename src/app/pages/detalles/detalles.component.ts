887801a7f83b6b4d7ccdcd9b8d496ce56fcb02fb
Michell2022 <sistemas.angel04@gmail.com>
2023-06-20 03:28:55 -0500

    Agregando proyecto

--- src/app/pages/detalles/detalles.component.ts
+++ src/app/pages/detalles/detalles.component.ts
@@ -8,14 +8,74 @@ import { PokemonesService } from 'src/app/services/pokemones.service';
   styleUrls: ['./detalles.component.css']
 })
 export class DetallesComponent implements OnInit {
+  pokemon: any = {}; // Objeto para almacenar los detalles del Pokémon
 
-  id!: number;
+  // Colores para los tipos de Pokémon
+  static colors: Record<string, string> = {
+    fire: '#e4604d',
+    grass: '#9dd465',
+    electric: '#fed32f',
+    water: '#0090ff',
+    ground: '#e4c967',
+    rock: '#cabb7b',
+    fairy: '#eeb2fa',
+    poison: '#9f619d',
+    bug: '#c5cf4a',
+    dragon: '#857af7',
+    psychic: '#e56eaf',
+    flying: '#80a4f9',
+    fighting: '#9b5a48',
+    normal: '#bab8ab',
+    steel: '#4682B4',
+    ghost: '#a499c9',
+    ice: '#00a4e3',
+  };
 
-  obtenerInfo:any = {};
-
-  constructor(private router:ActivatedRoute, private info:PokemonesService){ }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokemonesService: PokemonesService
+  ) { }
 
   ngOnInit(): void {
+    // Obtener el ID del Pokémon de los parámetros de la URL
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
 
+      // Obtener los detalles del Pokémon usando el servicio
+      this.pokemonesService.getPokemones(id).subscribe(
+        res => {
+          this.pokemon = {
+            id: res.id,
+            hp: res.stats[0].base_stat,
+            hpName: res.stats[0].stat.name,
+            image: res.sprites.other.dream_world.front_default,
+            name: res.name,
+            typeOfPokemon1: res.types[0].type.name,
+            typeOfPokemon2: res.types.length > 1 ? res.types[1].type.name : '',
+            attack: res.stats[1].base_stat,
+            defense: res.stats[2].base_stat,
+            speed: res.stats[5].base_stat,
+            attackName: res.stats[1].stat.name,
+            defenseName: res.stats[2].stat.name,
+            speedName: res.stats[5].stat.name
+          };
+        },
+        err => {
+          console.error(err);
+        }
+      );
+    });
+  }
+
+  guardarDatos() {
+    // Aquí puedes implementar el código para guardar los datos editados del Pokémon
+    console.log(this.pokemon);
+  }
+
+  getTextColor(typeOfPokemon: string): string {
+    if (typeOfPokemon && typeOfPokemon.toLowerCase) {
+      return DetallesComponent.colors[typeOfPokemon.toLowerCase()];
+    }
+    return '';
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,76 +9,104 @@ import { PokemonesService } from 'src/app/services/pokemones.service';
 })
 export class HomeComponent implements OnInit {
 
-  // DECLARO MI VARIABLE QUE RECIBIRA UNA COPIA DE LA LISTA ENTRADA
-  originalEntrada: any[] = [];
+    // Arreglo para almacenar los datos originales de los Pokémon
+    originalEntrada: any[] = [];
 
-  // ASIGNO LOS DATOS DE ORIGINALENTRADA A ENTRADA
-  entrada: any[] = [...this.originalEntrada];
+    // Arreglo para almacenar los datos filtrados de los Pokémon
+    entrada: any[] = [];
+  
+    // Variable para almacenar el valor del buscador
+    buscador = '';
 
-  // DECLARO MI VARIABLE BUSCADOR
-  buscador = '';
-
-  constructor(private mydata: PokemonesService, private router:Router) { }
-
-  ngOnInit(): void {
-    this.getPoketodo();
-
-    // this.mydata.getData().subscribe( mydata => this.entrada = mydata);
-  }
-
-
-  getPoketodo() {
-    let pokemonInfo;
-
-    // CREO UNA ESTRUCTURA DE CONTROL FOR PARA OBTENER SOLO 150 POKEMONES CON SUS RESPECTIVAS INFORMACIONES:
-    for (let i = 1; i <= 150; i++) {
-      this.mydata.getPokemones(Number(i)).subscribe(
-        res => {
-          pokemonInfo = {
-            // OBTENGO LOS DATOS POSICION - IMAGEN - NOMBRE Y EXPERIENCIA PARA EL SIGUIENTE NIVEL
-            position: i,
-            id: res.id,
-            image: res.sprites.front_default,
-            name: res.name,
-            experiencia: res.base_experience
-
+      // Colores para los tipos de Pokémon
+  static colors: Record<string, string> = {
+    fire: '#e4604d',
+    grass: '#9dd465',
+    electric: '#fed32f',
+    water: '#0090ff',
+    ground: '#e4c967',
+    rock: '#cabb7b',
+    fairy: '#eeb2fa',
+    poison: '#9f619d',
+    bug: '#c5cf4a',
+    dragon: '#857af7',
+    psychic: '#e56eaf',
+    flying: '#80a4f9',
+    fighting: '#9b5a48',
+    normal: '#bab8ab',
+    steel: '#4682B4',
+    ghost: '#a499c9',
+    ice: '#00a4e3',
+  };
+  
+    constructor(private mydata: PokemonesService, private router: Router) { }
+  
+    ngOnInit(): void {
+      // Obtener los datos de los Pokémon al inicializar el componente
+      this.getPoketodo();
+    }
+  
+    getPoketodo() {
+      for (let i = 1; i <= 150; i++) {
+        this.mydata.getPokemones(i).subscribe(
+          res => {
+            const pokemonInfo = {
+              position: i,
+              id: res.id,
+              image: res.sprites.other.dream_world.front_default,
+              name: res.name,
+              experiencia: res.base_experience,
+              tipoPoke: res.types[0].type.name
+            };
+  
+            // Agregar los datos del Pokémon al arreglo originalEntrada
+            this.originalEntrada.push(pokemonInfo);
+  
+            // Ordenar el arreglo originalEntrada por el ID del Pokémon
+            this.originalEntrada.sort((a, b) => a.id - b.id);
+  
+            // Copiar los datos originales al arreglo entrada
+            this.entrada = [...this.originalEntrada];
+          },
+          err => {
+            console.error(err);
           }
-          // LOS DATOS OBTENIDOS SERAN ASIGNADOS A MI VARIABLE ORIGINALENTRADA CON EL METODO PUSH
-          this.originalEntrada.push(pokemonInfo);
-          // DE IGUAL MANERA QUE AL INICIO ASIGNARE LOS DATOS DE ORIGINALENTRADA A ENTRADA
-          this.entrada = [...this.originalEntrada]
-        },
-        err => {
-          alert(err);
-        },
-      )
+        );
+      }
     }
-  }
-
-  // CREO MI FUNCION SEARCH 
-  search() {
-    // SI MI BUSCADOR NO TIENE INFORMACION - MI VARIABLE ORIGINALENTRADA ASIGNARA LOS DATOS A ENTRADA Y LOS MOSTRARA SOLO SI SE CUMPLE LA CONDICION
-    if (this.buscador === '') {
-      this.entrada = [...this.originalEntrada];
-      // FILTRANDO MIS ELEMENTOS
-      // toLowerCase() CONVERTIRA MI VALOR INGRESADO EN MINUSCULA.
-      // SI EL INDICE ES MAYOR QUE -1, SIGNIFICA QUE "this.buscador" se encuentra en "dato.name" Y LA FUNCION DEVUELVE true - EL ELEMENTO SE INCLUIRA dato.name
-      // EL INDICE -1, SIGNIFICA QUE "this.buscador" NO SE ENCUENTRA EN "dato.name" Y LA FUNCION DEVUELVE false - EL ELEMENTO NO SE INCLUIRA dato.name
-    } else {
-      this.entrada = this.originalEntrada.filter(dato =>
-        dato.name.toLowerCase().indexOf(this.buscador.toLowerCase()) > -1);
+  
+    search() {
+      if (this.buscador === '') {
+        // Si el buscador está vacío, mostrar todos los Pokémon
+        this.entrada = [...this.originalEntrada];
+      } else {
+        // Filtrar los Pokémon por el nombre ingresado en el buscador
+        this.entrada = this.originalEntrada.filter(dato =>
+          dato.name.toLowerCase().includes(this.buscador.toLowerCase())
+        );
+      }
     }
+  
+    sendData() {
+      // Enviar los datos del Pokémon seleccionado a la página de detalles
+      const selectedPokemon = this.originalEntrada.find(dato =>
+        dato.name.toLowerCase() === this.buscador.toLowerCase()
+      );
+  
+      if (selectedPokemon) {
+        this.router.navigate(['detalles', selectedPokemon.id]);
+      }
+    }
+  
+    seleccionarPokemon(dato: any) {
+      // Al seleccionar un Pokémon, asignar su nombre al buscador y realizar la búsqueda automáticamente
+      this.buscador = dato.name;
+      this.search();
+    }
+
+
+    getTextColor(tipoPoke: string): string {
+      return HomeComponent.colors[tipoPoke.toLowerCase()] || 'white';
+    }
+    
   }
-
-
-  // MI FUNCION sendData PASARA LOS DATOS DEL PRIMER POKEMON QUE ESCRIBA EN MI INPUT USANDO EL METODO FILTER 
-  sendData(index:string) {
-    const selectedPokemon = this.entrada.filter(dato =>
-    dato.name.toLowerCase().indexOf(index.toLowerCase()) > -1)[0];
-
-    this.router.navigate(['detalles',selectedPokemon.id]);
-    console.log(selectedPokemon);
-
-  }
-
-}
